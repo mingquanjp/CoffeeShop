c6064f9eccaa2cd638c9f57d889762277e1027b6
mingquanjp <faker1652005@gmail.com>
2025-03-28 21:17:10 +0700

    login and logout

--- cfshop/store/views.py
+++ cfshop/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def home(request):
         products = Product.objects.all()
@@ -8,8 +10,26 @@ def home(request):
 def about(request):
         return render(request,'about.html',{})
 
-def login(request):
-        return render(request,'login.html',{})
+def login_user(request):
+        if request.method == "POST":
+                username = request.POST['username']
+                password = request.POST['password']
+                user = authenticate(request,username = username, password = password)
+                if user is not None:
+                        login(request,user)
+                        messages.success(request,"You have been logged in!!!")
+                        return redirect('home')
+                else:
+                        messages.success(request,"There is an error, pls login again!!!")
+                        return redirect('login')
 
-def logout(request):
-        return render(request,'logout.html',{})
+
+
+
+        else:
+                 return render(request,'login.html',{})
+
+def logout_user(request):
+        logout(request)
+        messages.success(request,("You have been logged out ..."))
+        return redirect('home')
