4d42d37fc88efe7fb6d252fa014bf4fc0dc9a1c6
mingquanjp <faker1652005@gmail.com>
2025-03-28 10:24:28 +0700

    update

--- cfshop/store/views.py
+++ cfshop/store/views.py
@@ -5,3 +5,11 @@ def home(request):
         products = Product.objects.all()
         return render(request, 'home.html',{'products': products})
         
+def about(request):
+        return render(request,'about.html',{})
+
+def login(request):
+        return render(request,'login.html',{})
+
+def logout(request):
+        return render(request,'logout.html',{})
