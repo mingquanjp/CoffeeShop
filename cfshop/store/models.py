c408c0459f1c08cd7623ec58ef5b091bdba09cf3
mingquanjp <faker1652005@gmail.com>
2025-03-28 10:23:41 +0700

    update homehome

--- cfshop/store/models.py
+++ cfshop/store/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural ='categories'
 
 
 class Customer(models.Model):
@@ -20,10 +23,12 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    price = models.DecimalField(default=0, decimal_places=3, max_digits=8)
     category= models.ForeignKey(Category,on_delete=models.CASCADE, default=1)
     description=models.CharField(max_length=250, default='', blank=True, null = True)
     image=models.ImageField(upload_to='uploads/product/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=3, max_digits=8)
     
     def __str__(self):
         return self.name
