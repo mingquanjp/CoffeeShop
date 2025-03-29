a382775c60dfeb4e25ec742b27160bce0357f1e1
mingquanjp <faker1652005@gmail.com>
2025-03-30 00:45:13 +0700

    update product detail and cat

--- cfshop/store/urls.py
+++ cfshop/store/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name ='about'),
     path('login/',views.login_user,name ='login'),
-    path('logout/',views.logout_user,name ='logout')
+    path('logout/',views.logout_user,name ='logout'),
+    path('signup/',views.register, name = 'register'),
+    path('product/<int:pk>', views.product, name = 'product'),
+    path('category/<str:foo>', views.category, name ='category')
 ]
     
 
--- cfshop/store/views.py
+++ cfshop/store/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import redirect, render
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 # Create your views here.
+
+def product(request, pk):
+        product = Product.objects.get(id=pk)
+        return render(request, 'product.html',{'product': product} )
+
 def home(request):
         products = Product.objects.all()
         return render(request, 'home.html',{'products': products})
@@ -33,3 +42,39 @@ def logout_user(request):
         logout(request)
         messages.success(request,("You have been logged out ..."))
         return redirect('home')
+
+def register(request):
+        form = SignUpForm()
+        if request.method == "POST":
+                form = SignUpForm(request.POST)
+                if form.is_valid:
+                        form.save()
+                        username = form.cleaned_data['username']
+                        password = form.cleaned_data['password1']
+
+                        user = authenticate(username = username, password = password)
+                        login(request,user)
+                        messages.success(request, "You have Register success")
+                        return redirect ('home')
+                else:
+                        messages.success(request, "Oop, you have problem in Register. Pls register again!!!")
+                        return redirect ('register')
+        else:
+                return render(request,'register.html',{'form' : form})
+        
+
+
+def category(request,foo):
+        foo = foo.replace('-' , ' ')
+        try:
+                category = Category.objects.get(name=foo)
+                products = Product.objects.filter(category = category)
+                return render(request,'category.html',{'category': category, 'products' : products})
+
+        
+        except:
+                messages.success(request,"That category doesn't exist!!!")
+                return redirect('home')
+
+
+
