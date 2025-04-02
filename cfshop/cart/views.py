2d16a08daf079fad274c5432ed100330637b434c
mingquanjp <faker1652005@gmail.com>
2025-04-02 11:04:28 +0700

    change

--- cfshop/cart/views.py
+++ cfshop/cart/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart_summary(request):
-    return render(request,'cartsummary',{})
+    return render(request,'cart_summary.html',{})
+
+
+def cart_add(request):
+   # return render(request,'cart_add',{})
+   pass
+
+
+def cart_delete(request):
+    #return render(request,'cart_delete',{})
+    pass
+
+def cart_update(request):
+    #return render(request,'cart_update',{})
+    pass
